9e86d246b9e02193e0b85b069a113cb159e42f6b
Krishna T <krishnatummeti@gmail.com>
2025-05-04 19:51:47 +0530

    Add flask app dir

--- Flask_App/app.py
+++ Flask_App/app.py
@@ -1,14 +1,13 @@
 from flask import Flask
+import redis
 
 app = Flask(__name__)
+redis_client = redis.StrictRedis(host='redis', port=6379, db=0)
 
 @app.route('/')
-def home():
-    return "Hello from Flask inside Docker!"
-
-@app.route('/hello/<name>')
-def hello(name):
-    return f"Hello, {name}!"
+def hello_world():
+    redis_client.incr('hits')
+    return f'Hello, World! You have visited this page {redis_client.get("hits").decode("utf-8")} times.\n'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
